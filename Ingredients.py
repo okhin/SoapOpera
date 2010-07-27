6483c9f118dacf483e2debc31d6f8981e893d234
okhin <okhin@okhin.fr>
2010-07-27 17:24:03 +0200

    Calcul de la sat/unsat + indices

--- Ingredients.py
+++ Ingredients.py
@@ -8,7 +8,7 @@ class Ingredient:
 		this.famille = famille
 		this.nom = nom
 	def nom(this):
-		"""Si le nom lation a une longueur nulle, c'est qu'il n'est pas déini, donc on retourne le nom vulgaire"""
+		"""Si le nom lation a une longueur nulle, c'est qu'il n'est pas défini, donc on retourne le nom vulgaire"""
 		return this.latin if len(this.latin) > 0 else this.nom
 	def operator.__eq__(this, other):
 		"""Un comparateur d'ingredients, pour vérifier si deux composants sont les mêmes. On vérifie les references des objets"""
@@ -21,12 +21,39 @@ class Gras(Ingredient):
 		Ingredient.__init__(this, nom, stock)
 		this.sap = sap
 		this.ins = ins
+		this.saturation()
+
 	def soude(this):
 		"""Calcul la quantite de soude par gramme de gras
 		La sap est à multiplier par 0,713 pour avoir la quantite de soude par kilo de gras"""
 		return this.sap * 0.713/1000
+
 	def getIndice(this, indice):
-		"""On récupère la valeur d'un indice particulier. Si il n'est pas definit, c'ets qu'il vaut 0"""
+		"""On récupère la valeur d'un indice particulier. Si il n'est pas definit, c'est qu'il vaut 0"""
 		if indice in this.indices.keys():
 			return this.indices(indice)
 		return 0
+
+	def saturation(this):
+		this.sat = 0
+		this.unsat = 0
+		for (indice, quantite) in this.indices:
+			if indice.sat:
+				this.sat += indice.ratio * quantite
+			else
+				this.unsat += indice.ratio * quantite
+
+class Indice():
+	"""Les indices de saturation ont besoin d'un peu de boulot avant de pouvoir être intégré.
+	la valeur sat définit si il s'agit d'un indice Saturé/Polysaturé/Monosaturé whatever.
+	Si elle est à 1 on ajoute à la Sat, sinon à l'Unsat"""
+	def __init__(this, nom, sat, ratio):
+		this.nom = nom
+		if sat > 0:
+			this.sat = 1
+		else
+			this.sat = 0
+		this.ratio = ratio
+	def __eq__(this, other):
+		if other.isinstance(Ingredients.Indice):
+			return this.nom == other.nom	
--- Recettes.py
+++ Recettes.py
@@ -4,11 +4,26 @@ class Recette:
 	"""Une classe pour stocker les recettes"""
 	surgraissage = 0.08
 	soude = 0
-	eau = 0
+	concentration = 0.35
 	volume = 1000
 	def __init__(self, gras, he, additifs):
-		this.gras = [x for x in gras if isinstance(x, Ingredients.Gras)]
+		"""Les arguments sont des listes de tuples avec ratio/ingredients"""
+		this.gras = [(x,v) for (x,v) in gras if v.isinstance(Ingredients.Gras)]
 		this.he = he
 		this.additifs = additifs
 	def soude(self):
 		this.soude = sum([gras[0].soude()*gras[1]*self.volume for gras in self.gras])*(1 - this.surgraissage)
+
+	def eau(self):
+		"""Concentration = taux de soude pour le volume total (graisse + eau).
+		Donc, graisse + eau = taux de soude / concentration
+		Donc eau = taux de soude / concentration - graisse."""
+		this.eau = this.soude / this.concentration - this.volume
+
+	def satUnsat(self):
+		# On commence par rafraîchir les gras de la recette
+		this.sat = this.unsat = 0
+		for (gras,taux) in this.gras:
+			gras.saturation()
+			this.sat += gras.sat * taux if gras.sat == 1
+			this.unsat += gras.unsat * taux if gras.sat == 0
