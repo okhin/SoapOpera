faf7e9d126c732f3ce97603b5a6666bcdaf10d66
okhin <okhin@okhin.fr>
2010-08-03 19:02:33 +0200

    Modules ARE Object you dumbass. Easier like that.

--- Csv.py
+++ Csv.py
@@ -13,47 +13,13 @@ import sys
 import Ingredients
 import Recettes
 
-class csvHelper():
-	"""Un helper générique de fichier CSV"""
-	def __init__(this, filename):
-		try:
-			f = open(filename, 'wb')
-			this.filename = filename
-
-		except IOError, e:
-			sys.exit("file %s: %s" % (filename, e))
-
-		finally:
-			close(f)
-
-	def search(this, key, value):
-		"""On cherche un élément dans une clef particulière"""
-		csvReader = csv.DictReader(open(this.filename, 'rb'))
-		try:
-			for row in csvReader:
-				if key is not in row.keys():
-					raise NOKey
-
-				if row[key] == value:
-					return row
-
-		except csv.Error, e:
-			sys.exit("file %s, line %d: %s" % (this.filename, csvReader.line_num, e))
-		# If not found, return none	
-		return None
-
-	def loadIngredient(this, name):
-		"""On charge un ingredient depuis un dictionnaire"""
-		try:
-			result = this.search(name)
-			ingredient = Ingredients.Ingredient(result['ref'], result['famille'], result['nom'])
-			return ingredient
-
-		except NOKey, e:
-			sys.exit("file %s, name %s" % (this.filename, name, e))
-
-	def loadGras(this, name):
-		"""On charge un gras depuis un dictionnaire"""
-		try:
-			result = this.search(name)
-			gras = Ingredients.Gras(result['ref'],
+def load(filename, key, value):
+	"""On charge un élément depuis un fichier particulier en cherchant une valeur dans une clef
+	On retourne un dictionnaire de résultat ou une liste vide (None)."""
+	reader = csv.DictReader(open(filename, 'rb'))
+	try:
+		return [row for row in reader if row[key] == value]
+	except csv.Error, e:
+		sys.exit("file %s, line %d: %s" % (filename, csvReader.line_num, e))
+	# If not found, return none	
+	return None
