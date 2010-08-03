e40188e8a7a4477ba6c7962f7c8edd82c51ad638
okhin <okhin@okhin.fr>
2010-08-03 13:51:53 +0200

    Working on Databases and CSV files

--- Databases.py
+++ Databases.py
@@ -3,6 +3,7 @@
 # Classes and function usefull for SQLite interaction
 # System imports
 from pysqlite2 import dbapi2 as sqlite
+import os.path
 
 # Applicatives Import
 import Ingredients
@@ -11,22 +12,40 @@ import Recettes
 connection = sqlite.connect('soap_opera.db')
 cursor = connection.cursor()
 
-# Table Ingredient qui contient toutes les donnees generiques
-cursor.execute('CREATE TABLE "Ingredients" (ref VARCHAR(50) PRIMARY KEY, famille VARCHAR(50), nom VARCHAR(50))')
-connection.commit()
+def InitAppli():
+	# Table Ingredient qui contient toutes les donnees generiques
+	cursor.execute('CREATE TABLE "Ingredients" (ref VARCHAR(50) PRIMARY KEY, famille VARCHAR(50), nom VARCHAR(50))')
+	connection.commit()
 
-# Table AcideGras qui contient les compositions en Acide Gras, la SAP et l'INS des gras utilisés.
-# Lien avec la table Ingredients par la reference.
-cursor.execute('CREATE TABLE "AcideGras" (ref VARCHAR(50) PRIMARY KEY, ins NUMBER, sap NUMBER, lauric NUMBER)')
-connection.commit()
+	# Table AcideGras qui contient les compositions en Acide Gras, la SAP et l'INS des gras utilisés.
+	# Lien avec la table Ingredients par la reference.
+	cursor.execute('CREATE TABLE "AcideGras" (ref VARCHAR(50) PRIMARY KEY, ins NUMBER, sap NUMBER, lauric NUMBER)')
+	connection.commit()
 
-# Table de saturation deux champs, nom (unique) et boolean saturation
-cursor.execute('CREATE TABLE "Saturation" (nom VARCHAR(50) PRIMARY KEY, saturation BOOLEAN)')
-connection.commit()
+	# Table de saturation deux champs, nom (unique) et boolean saturation
+	cursor.execute('CREATE TABLE "Saturation" (nom VARCHAR(50) PRIMARY KEY, saturation BOOLEAN)')
+	connection.commit()
 
-# Feed in some dummy datas
-cursor.execute('INSERT INTO "Ingredients" VALUES ("1", "Gras", "UnGras")')
-cursor.execute('INSERT INTO "AcideGras" VALUES ("1", 123, 287, 44)')
-cursor.execute('INSERT INTO "Saturation" VALUES ("lauric", true)')
-connection.commit()
+	# Feed in some dummy datas
+	cursor.execute('INSERT INTO "Ingredients" VALUES ("1", "Gras", "UnGras")')
+	cursor.execute('INSERT INTO "AcideGras" VALUES ("1", 123, 287, 44)')
+	connection.commit()
 
+# Import Saturation from csv
+def CSV_Saturation(filename):
+	try:
+		f = open(filename,'rb', 0)
+		try:
+			for lines in f.readlines():
+				parts = lines.split(';')
+				cursor.execute('INSERT INTO Saturation VALUES (?, ?)', (parts[0], parts[1]))
+				connection.commit
+		finally:
+			f.close()
+
+	except IOError:
+		pass
+
+def SELECT_Saturation():
+	cursor.execute('SELECT * FROM Saturation')
+	print cursor.fetchall()
